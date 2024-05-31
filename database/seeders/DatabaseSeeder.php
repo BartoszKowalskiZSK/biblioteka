e452e19898cba49209c0012255205b48486cb4b0
unknown <f.mikolajczak25@gmail.com>
2024-05-31 17:10:19 +0200

    nasienie użytkowników

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,14 +14,15 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(NasienieAutorzy::class);
         $this->call(NasienieBooks::class);
+        $this->call(UsersTableSeeder::class); // Dodane
 
         // User::factory(10)->create();
 
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
-            'password' => 'test',
-            'privillages' => '5',
+            'password' => bcrypt('test'), // Hashowanie hasła
+            'privillages' => '5', // Zmiana na ciąg znaków
             'amount' => 3
         ]);
     }
